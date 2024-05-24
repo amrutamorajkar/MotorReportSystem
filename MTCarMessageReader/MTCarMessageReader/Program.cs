8e1f8c769645c70b7af5b18335a483c23198afd6
Amruta Morajkar <amruta.morajkar@gmail.com>
2024-05-23 21:40:37 -0400

    Message Reader project

---
+++ MTCarMessageReader/MTCarMessageReader/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Messaging.ServiceBus;
+
+// Azure service bus client
+ServiceBusClient client;
+
+// the processor that reads and processes messages from the subscription
+ServiceBusProcessor processor;
+
+string topicName = "MtMotorsTopic";
+string subscriberName = "MtMotorsSubt";
+string connectionString = "Endpoint=sb://mtmotorssub.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=crTDHxHLDysrKDz/IwZ7ilj9aIKpb2Foe+ASbLaCySM=";
+
+// handle received messages
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($"Received: {body} from subscription: {subscriberName}");
+    Console.WriteLine();
+
+    // complete the message. messages is deleted from the subscription. 
+    await args.CompleteMessageAsync(args.Message);
+}
+
+// handle any errors when receiving messages
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
+
+// The Service Bus client types are safe to cache and use as a singleton for the lifetime
+// of the application, which is best practice when messages are being published or read
+// regularly.
+client = new ServiceBusClient(connectionString);
+
+// create a processor that we can use to process the messages
+processor = client.CreateProcessor(topicName,subscriberName, new ServiceBusProcessorOptions());
+
+try
+{
+    // add handler to process messages
+    processor.ProcessMessageAsync += MessageHandler;
+
+    // add handler to process any errors
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    // start processing 
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("Wait for a minute and then press any key to end the processing");
+    Console.ReadKey();
+
+    // stop processing 
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
+}
+finally
+{
+    // Calling DisposeAsync on client types is required to ensure that network
+    // resources and other unmanaged objects are properly cleaned up.
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
